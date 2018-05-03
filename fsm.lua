036c2f783be3bf86757fd2985bbdf2311b1ae8ea
Lukas <kodlc507@gmail.com>
2018-05-03 19:01:49 +0200

    Update fsm.lua
    Added some comments

--- fsm.lua
+++ fsm.lua
@@ -3,6 +3,12 @@ fsm = {}
 local testmsg = ""
 
 fsm.states = {
+    ['MODEL STATE'] = function(dt)
+        -- this WHAT HAPPENS when in this state
+        -- it is also WHAT CAN HAPPEN when in this state
+    end,
+    
+    -- here's a couple of sample states
     ['idle'] = function(dt)
         testmsg = "you are in an idle state. press space to enter an action state."
         if love.keyboard.isDown("space") then 
@@ -18,10 +24,10 @@ fsm.states = {
     end
 }
 
-fsm.cur_state = 'idle'
+fsm.cur_state = 'idle' -- current state
 
 function fsm:update(dt)
-    self.states[self.cur_state](dt)
+    self.states[self.cur_state](dt) -- update the state you are currently in
     print(self.cur_state, testmsg)
 end
 
